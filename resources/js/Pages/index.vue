607add52e58591104a5b8babe58894ffb41551ea
zaarza <zaarza03@gmail.com>
2023-05-05 16:47:34 +0700

    Initial setup

---
+++ resources/js/Pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(["name"]);
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="container__title">Hello {{ name }}</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    &__title {
+        color: blue;
+    }
+}
+</style>
