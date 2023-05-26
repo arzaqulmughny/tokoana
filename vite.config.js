77a561201cf35bfc351e662755e8df0626ce9b2a
zaarza <zaarza03@gmail.com>
2023-05-26 20:45:28 +0700

    Fix props type

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => ["MainLayout"].includes(tag),
+                },
+            },
+        }),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
